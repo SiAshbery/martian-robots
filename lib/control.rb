7d802cb01cd6a0acf729cb129b76479cdb1edd13
Simon Ashbery <si.ashbery@gmail.com>
2018-10-14 18:14:37 +0100

    control can launch a mission

--- lib/control.rb
+++ lib/control.rb
@@ -1,3 +1,9 @@
+require 'surface'
 class Control
+    attr_reader :surface
+
+    def launch_mission(x_coord, y_coord)
+        @surface = Surface.new(x_coord, y_coord)
+    end
    
 end
--- spec/control_spec.rb
+++ spec/control_spec.rb
@@ -1,4 +1,5 @@
 require 'control'
+require 'surface'
 
 describe Control do
     subject(:control) { described_class.new }
@@ -6,6 +7,14 @@ describe Control do
     describe '#initialize' do
         it 'Exists' do
             expect(control).to be_instance_of(described_class)
+        end
+    end
+
+    describe '#launch_mission' do
+        it 'creates a surface object' do
+            expect(control.surface).to be_nil
+            control.launch_mission(5,5)
+            expect(control.surface).to be_instance_of(Surface)
         end
     end
 
